9bfe8f1b8ae1109b1c3f36f3d907ae24462d5e60
Jimmy Beckett <jimmybeckett1@gnail.com>
2018-03-06 09:39:55 -0600

    Initial Commit

---
+++ LaNIX/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LaNIX
+//
+//  Created by Jimmy Beckett on 3/6/18.
+//  Copyright © 2018 Jimmy Beckett. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
